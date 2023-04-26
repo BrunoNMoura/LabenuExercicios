459a252454eb6e25d06fe8f1b8c565ddc9587888
BrunoNMoura <bnmalemao@gmail.com>
2023-04-25 21:01:23 -0300

    exercícios e template sobre estado global-react

---
+++ react/estado-global-template-main/estado-global-template-main/src/App.js
@@ -0,0 +1,15 @@
+import Router from "./Router/Router";
+import styled from "styled-components";
+import React from "react";
+
+export default function App() {
+  return (
+    <AppContainer>
+      <h1 id="frutinhas">Labefrutas <span role="img" aria-label="frutinhas">🍌 🍉 🍇 🍓 </span></h1>
+      <Router />
+    </AppContainer>
+  );
+}
+const AppContainer = styled.div`
+   text-align: center;
+`;
